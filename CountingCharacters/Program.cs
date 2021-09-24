44e6ef73ce314b707ed3a0b1c28f74370c5755ef
Willt617 <85527300+Willt617@users.noreply.github.com>
2021-09-23 21:09:44 -0400

    finished counting character

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -7,27 +7,34 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
-            string test = "William Thompson";
-            char[] charactersInString = test.ToCharArray();
-            Console.WriteLine(charactersInString);
-
+            Console.WriteLine("Plese Enter Word Whose Letters You Would Like To Count!");
+            string input = Console.ReadLine();
+            characterCount(input);          
+        }
+        public static bool isAlphabetic(char c)
+        {
+            return (c >= 'a' && c <= 'z');
+        }
+        public static Dictionary<char,int> characterCount(string str)
+        {
+            char[] charactersInString = str.ToLower().ToCharArray();
             Dictionary<char, int> charCount = new Dictionary<char, int>();
 
             foreach (char letter in charactersInString)
             {
+                if (!isAlphabetic(letter))
+                    continue;
                 if (charCount.ContainsKey(letter))
-                {
                     charCount[letter] += 1;
-                }
                 else
-                {
                     charCount.Add(letter, 1);
-                }
             }
-            foreach(KeyValuePair<char,int> item in charCount)
+            foreach (KeyValuePair<char, int> item in charCount)
             {
                 Console.WriteLine($"{item.Key}: {item.Value}");
             }
+
+            return charCount;
         }
     }
 }
